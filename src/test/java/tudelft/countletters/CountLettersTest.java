f024f0396081bd579d4bee19705ca36a7ec03650
Kevin Groot <kevin.groot@hollandcasino.nl>
2021-02-16 10:09:32 +0100

    Topic 3 - Lesson 2 - Finished

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class CountLettersTest {
 
     @Test
-    public void multipleMatchingWords() {
+    public void multipleMatchingWordsWithS() {
         int words = new CountLetters().count("cats|dogs");
         Assertions.assertEquals(2, words);
     }
@@ -15,6 +15,18 @@ public class CountLettersTest {
     public void lastWordDoesNotMatch() {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void multipleMatchingWordsWithR() {
+        int words = new CountLetters().count("car|door");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void wordsDoNotMatch() {
+        int words = new CountLetters().count("bike|airplane");
+        Assertions.assertEquals(0, words);
     }
 
 }
