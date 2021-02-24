40d3127d5d361d822dd3f9ee5b434a814312e6cd
Kevin Groot <kevin.groot@hollandcasino.nl>
2021-02-24 10:25:14 +0100

    Topic 4 - Lesson 2 - Finished -> updated the Mockito dependency

--- src/test/java/tudelft/invoicemocked/InvoiceFilterTest.java
+++ src/test/java/tudelft/invoicemocked/InvoiceFilterTest.java
@@ -3,7 +3,6 @@ package tudelft.invoicemocked;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
 import java.util.Arrays;
 import java.util.List;
 
