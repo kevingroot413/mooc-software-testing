48a57380e75ee6b41a7d88536a74790ca8b515f3
Groot <kevin.groot@hollandcasino.nl>
2021-03-13 15:59:27 +0100

    Finished complete course

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,49 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    private MirrorSolution mirror;
+
+    @BeforeEach
+    public void initialize() {
+        this.mirror = new MirrorSolution();
+    }
+
+    @Test
+    public void evenString(){
+        String result = mirror.mirrorEnds("ab");
+
+        Assertions.assertEquals("", result);
+    }
+
+    @Test
+    public void evenString2(){
+        String result = mirror.mirrorEnds("abca");
+
+        Assertions.assertEquals("a", result);
+    }
+
+    @Test
+    public void unevenString(){
+        String result = mirror.mirrorEnds("abc");
+
+        Assertions.assertEquals("", result);
+    }
+    @Test
+    public void unevenString2(){
+        String result = mirror.mirrorEnds("abXYZba");
+
+        Assertions.assertEquals("ab", result);
+    }
+
+    @Test
+    public void unevenString3(){
+        String result = mirror.mirrorEnds("aba");
+
+        Assertions.assertEquals("aba", result);
+    }
 }
